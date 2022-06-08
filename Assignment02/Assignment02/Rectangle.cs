8656583d5c6ee02e084b69a8fd74dad533fe79e2
Rashmi Anchan <Ranchan6650@conestogac.on.ca>
2022-06-08 16:18:13 -0400

    Completed rectangle class

---
+++ Assignment02/Assignment02/Rectangle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    public class Rectangle
+    {
+        private int? length, width;
+        public Rectangle()
+        {
+            this.length = 1;
+            this.width = 1;
+        }
+        public Rectangle(int? length, int? width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public int? GetLength()
+        {
+            return this.length;
+        }
+        public int? SetLength(int? length)
+        {
+            this.length = length;
+            return this.length;
+        }
+
+        public int? GetWidth()
+        {
+            return this.width;
+        }
+        public int? SetWidth(int? width)
+        {
+            this.width = width;
+            return this.width;
+        }
+        public int? GetPerimeter()
+        {
+            try
+            {
+                return (this.length + this.width) * 2;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+
+        }
+        public int? GetArea()
+        {
+            try
+            {
+                return this.length * this.width;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+
+        }
+    }
+}
